5316c84136d08491b856d364ed5fb46b02f0bb95
salihcandusmez <salihcandusmez@gmail.com>
2022-03-22 13:05:40 -0700

    header created

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,23 +2,14 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import logo from '../assets/logo@2x.png';
 
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+      <AppBar position="static" sx={{ backgroundColor: '#F4F4F4', boxShadow: 0 }}>
+        <Toolbar variant="regular" sx={{ justifyContent: 'center' }}>
+          <img src={logo} alt="Logo" width={113} height={34} />;
         </Toolbar>
       </AppBar>
     </Box>
