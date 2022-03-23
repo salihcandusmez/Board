22fcf605a42cba69504875ba3a760ecfa5e96b70
salihcandusmez <salihcandusmez@gmail.com>
2022-03-22 23:02:44 -0700

    fix docker

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
-FROM node: alpine
-# Add a work directory
+# build environment
+FROM node:lts-alpine as build
 WORKDIR /app
-# Cache and Install dependencies
-COPY package.json .
-COPY yarn.lock .
-RUN yarn install
-# Copy app files
-COPY . .
-# Expose port
-EXPOSE 3000
-# Start the app
-CMD [ "yarn", "start" ]
+ENV PATH /app/node_modules/.bin:$PATH
+COPY package.json ./
+COPY package-lock.json ./
+RUN npm ci --silent
+RUN npm install react-scripts@5.0.0 -g --silent
+COPY . ./
+RUN npm run build
+
+# production environment
+FROM nginx:stable-alpine
+COPY --from=build /app/build /usr/share/nginx/html
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,31 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import logo from '../assets/logo@2x.png';
+import { IconButton } from '@mui/material';
+import LanguageIcon from '@mui/icons-material/Language';
+import { useTranslation } from 'react-i18next';
 
 export default function ButtonAppBar() {
+  const { i18n } = useTranslation();
+
+  const changeLanguage = () => {
+    if (i18n.language === 'tr') {
+      i18n.changeLanguage('en-Us');
+    } else {
+      i18n.changeLanguage('tr');
+    }
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{ backgroundColor: '#F4F4F4', boxShadow: 0, marginBottom: 9 }}>
-        <Toolbar variant="regular" sx={{ justifyContent: 'center' }}>
-          <img src={logo} alt="Logo" width={113} height={34} />;
+        <Toolbar variant="regular">
+          <Box sx={{ display: 'flex' }} justifyContent="center" flex={2}>
+            <img src={logo} alt="Logo" width={113} height={34} />
+          </Box>
+          <IconButton size="large" onClick={changeLanguage} sx={{ ml: 'auto' }}>
+            <LanguageIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </Box>
